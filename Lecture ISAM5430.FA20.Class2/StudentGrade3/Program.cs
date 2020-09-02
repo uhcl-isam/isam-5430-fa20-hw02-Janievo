abcf2e374276c96e9ca563d49857ba81135b02fa
JV <janievo.mt@gmail.com>
2020-09-01 23:40:04 -0500

    Day 2

---
+++ Lecture ISAM5430.FA20.Class2/StudentGrade3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StudentGrade3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal total = 0m;
+
+            int i = 0;
+            while (i<10) //enter limit to 10 scores
+            {
+                i++;
+                Console.WriteLine($"Enter the score for student #{i}.");
+                decimal score = decimal.Parse(Console.ReadLine());
+                if (score <0 || score >100)
+                {
+                    Console.Error.WriteLine("Invalid score, please enter again");
+                    i--;
+                }
+                else
+                {
+                    total += score;
+                }
+            }
+            decimal average = total / 10;
+            Console.WriteLine($"The average is {average}.");
+        }
+    }
+}
---
+++ Lecture ISAM5430.FA20.Class2/StudentGrade4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks.Sources;
+
+namespace StudentGrade4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal total = 0m;
+
+            int i = 0;
+            decimal score = 0;
+            //enter a list of scores until one of the score is negative.
+            while (score >= 0) //enter as many score as you want 
+            {
+                i++;
+                Console.WriteLine($"Enter the score for student #{i}.");
+                score = decimal.Parse(Console.ReadLine());
+                if (score >= 0)
+                {
+                    if (score > 100)
+                    {
+                        Console.Error.WriteLine("Invalid score, please enter again");
+                        i--;
+                    }
+                    else
+                    {
+                        total += score;
+                    }
+                }
+            }
+            if (i > 0)
+            {
+                decimal average = total / (i - 1);
+                Console.WriteLine($"The average is {average:N2}.");
+            }
+            else
+            {
+                Console.WriteLine("You have not enter any score");
+            }
+        }
+    }
+}
