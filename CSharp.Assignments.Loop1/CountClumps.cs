65ec11090949f9a2f77ed225e707563935454625
JV <JV@JANIEVO90E5>
2020-09-13 17:07:03 -0500

    Assignment 2

--- CSharp.Assignments.Loop1/CountClumps.cs
+++ CSharp.Assignments.Loop1/CountClumps.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,22 @@ namespace CSharp.Assignments.Loop1
         public static void Main()
         {
             // Write your codes here
+            Console.Error.Write("Enter a counter: ");
+            int counter = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            Console.Error.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i =0; i <= counter; i++)
+            {
+                if (i == num)
+                {
+                    count =+ 1;
+                }
+                count =+ 0;
+            }
+            Console.WriteLine(count);
         }
     }
 }
